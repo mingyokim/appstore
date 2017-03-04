7bb4f243b837d39f43d709431db70cf0da08a5d7
Min Gyo Kim <mingyokim@Mins-iMac.local>
2017-03-04 12:03:16 -0800

    Initial Commit

---
+++ appstore/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  appstore
+//
+//  Created by Min Gyo Kim on 2017-03-04.
+//  Copyright © 2017 Min Gyo Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
